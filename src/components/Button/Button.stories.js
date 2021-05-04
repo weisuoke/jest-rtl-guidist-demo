3fd214f5e653f24b27169483ccb4a5c579fd44d8
梅林的法杖 <530264127@qq.com>
2021-05-04 22:50:00 +0800

    fix: ISSUE-1

--- src/components/Button/Button.stories.js
+++ src/components/Button/Button.stories.js
@@ -7,4 +7,21 @@ import Button from "./index";
 export default {
   title: "Components/Button",
   Component: Button,
+  argTypes: {
+    onClick: {
+      action: "clicked",
+    },
+  },
+};
+
+const Template = (args) => (
+  <Button {...args} onClick={args.onClick}>
+    {args.children}
+  </Button>
+);
+
+export const LargeButton = Template.bind({});
+LargeButton.args = {
+  children: "LargeButton",
+  size: "large",
 };
